5fc62835311edb599e89247beb9e817e5185babf
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-02-01 18:41:23 -0300

    feat: created initial html structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import styles from './App.module.css';
+import logoPath from './assets/todo-list-logo.svg';
 
 function App(): React.ReactElement {
-  return <h1>ToDo List</h1>;
+  return (
+    <>
+      <header className={styles.header}>
+        <img src={logoPath} alt="ToDo List logo" title="ToDo List logo" />
+      </header>
+
+      <form className={styles.form}>
+        <input></input>
+        <button></button>
+      </form>
+
+      <div className={styles.tasksWrapper}>
+        <header className={styles.tasksInfo}>
+          <p className={styles.createdTasks}>
+            <span></span>
+          </p>
+          <p className={styles.completedTasks}>
+            <span></span>
+          </p>
+        </header>
+
+        <div className={styles.tasksList}></div>
+      </div>
+    </>
+  );
 }
 
 export default App;
